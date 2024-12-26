8ba2c1497f76658aa4109d660d9578ae5c36ac63
Erismann Matteo <matteo.erismann@student.vinci.be>
2024-12-26 12:09:44 +0100

    new:ex2.1-destructuring

--- B_exercices/2/2.1/src/components/Counter/Counter.jsx
+++ B_exercices/2/2.1/src/components/Counter/Counter.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 
-const Counter = () => {
+const Counter = ({ title, message }) => {
     const [count, setCount] = useState(0)
     
     return (
         <div className="card">
+        <h4>{title}</h4>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>
+          {count > 10 ? message : ""}
+        </p>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
