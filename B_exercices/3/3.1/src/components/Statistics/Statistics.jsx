a2bcc6ec39ea6dfffafcbbf1e75eed8c45db52d1
Erismann Matteo <matteo.erismann@student.vinci.be>
2024-12-27 12:59:21 +0100

    new:3.1-1.7

--- B_exercices/3/3.1/src/components/Statistics/Statistics.jsx
+++ B_exercices/3/3.1/src/components/Statistics/Statistics.jsx
@@ -7,6 +7,9 @@ const Statistics = ({good, neutral, bad}) => {
             <StatLine stat="Good" value={good} />
             <StatLine stat="Neutral" value={neutral} />
             <StatLine stat="Bad" value={bad} />
+            <StatLine stat="All" value={good + neutral + bad} />
+            <StatLine stat="Average" value={good !== 0 || neutral !== 0 || bad !== 0 ? (good - bad) / (good + neutral + bad) : 0} />
+            <StatLine stat="Positive" value={good !==0 || neutral !== 0 || bad !==0 ? (good / (good + neutral + bad)) * 100 + "%" : 0} />
         </div>
     )
 }
