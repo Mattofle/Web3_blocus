5ed80fe42e56d2cb3044eadff6567996f7520a4a
Erismann Matteo <matteo.erismann@student.vinci.be>
2024-12-27 14:06:20 +0100

    module3 : collection

---
+++ B_exercices/3/3.2/src/components/Part/Part.jsx
@@ -0,0 +1,18 @@
+
+
+const Part = ({ parts }) => {
+
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+          
+        </p>
+      ))}
+
+    </div>
+  );
+};
+
+export default Part;
