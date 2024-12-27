0451e21ce5c0e4aa45d1063259112ace733adc98
Erismann Matteo <matteo.erismann@student.vinci.be>
2024-12-27 13:04:11 +0100

    -new:3.1-1.9

--- B_exercices/3/3.1/src/components/Statistics/Statistics.jsx
+++ B_exercices/3/3.1/src/components/Statistics/Statistics.jsx
@@ -1,6 +1,15 @@
 import StatLine from "components/StatLine/StatLine";
 
 const Statistics = ({good, neutral, bad}) => {
+    if (good + neutral + bad === 0) {
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <p>No feedback given</p>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h1>Statistics</h1>
@@ -8,8 +17,14 @@ const Statistics = ({good, neutral, bad}) => {
             <StatLine stat="Neutral" value={neutral} />
             <StatLine stat="Bad" value={bad} />
             <StatLine stat="All" value={good + neutral + bad} />
-            <StatLine stat="Average" value={good !== 0 || neutral !== 0 || bad !== 0 ? (good - bad) / (good + neutral + bad) : 0} />
-            <StatLine stat="Positive" value={good !==0 || neutral !== 0 || bad !==0 ? (good / (good + neutral + bad)) * 100 + "%" : 0} />
+            {/* 
+                je laisse mais opérateurs ternaires ici pcq j'en étais content :'(
+                good !== 0 || neutral !== 0 || bad !== 0 ? (good - bad) / (good + neutral + bad) : 0
+                good !==0 || neutral !== 0 || bad !==0 ? (good / (good + neutral + bad)) * 100 + "%" : 0
+             */}
+            
+            <StatLine stat="Average" value={(good - bad) / (good + neutral + bad)} />
+            <StatLine stat="Positive" value={(good / (good + neutral + bad)) * 100 + "%"} />
         </div>
     )
 }
